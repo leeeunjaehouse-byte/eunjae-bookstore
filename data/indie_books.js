38902dd741cd409ddd71be4ea85be1cdfbf612ec
leeeunjaehouse-byte <leeeunjae.house@gmail.com>
2026-07-30 08:03:12 +0900

    auto: 2026-07-30 국내·해외 신간 자동 갱신

--- data/indie_books.js
+++ data/indie_books.js
@@ -1,6 +1,6 @@
 window.__INDIE_BOOKS_DATA__ = {
-  "updated_at": "2026-07-29T08:02:30.513988+09:00",
-  "today": "2026-07-29",
+  "updated_at": "2026-07-30T08:02:38.025685+09:00",
+  "today": "2026-07-30",
   "books": [
     {
       "title": "돌고래 지망생 · 안유진",
@@ -73,6 +73,15 @@ window.__INDIE_BOOKS_DATA__ = {
       "store_cover": "https://www.your-mind.com/web/product/big/202606/d43337001e940c60b173c50f4d931024.jpg",
       "cover": "https://www.your-mind.com/web/product/big/202606/d43337001e940c60b173c50f4d931024.jpg",
       "aladin_search": "https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord=%5B%EC%9E%AC%EC%9E%85%EA%B3%A0%5D%20%EC%8B%A4%EC%88%98%20%EC%88%98%EC%A7%91%EA%B0%80%20%C2%B7%20%EA%B9%80%EC%88%98%EC%A0%95"
+    },
+    {
+      "title": "[4차 입고] 꽃점 (Floral Fortune) · 희다가든",
+      "store": "유어마인드",
+      "section": "BOOKS",
+      "store_link": "https://www.your-mind.com/product/4차-입고-꽃점-floral-fortune-·-희다가든/6147/category/27/display/1/",
+      "store_cover": "https://www.your-mind.com/web/product/big/202606/4e6f1f1d9018822d796ec3d49092bd60.png",
+      "cover": "https://www.your-mind.com/web/product/big/202606/4e6f1f1d9018822d796ec3d49092bd60.png",
+      "aladin_search": "https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord=%5B4%EC%B0%A8%20%EC%9E%85%EA%B3%A0%5D%20%EA%BD%83%EC%A0%90%20%28Floral%20Fortune%29%20%C2%B7%20%ED%9D%AC%EB%8B%A4%EA%B0%80%EB%93%A0"
     },
     {
       "title": "Sunny Bier Klub(Re-Design) · 이하여백",
@@ -262,15 +271,6 @@ window.__INDIE_BOOKS_DATA__ = {
       "store_cover": "https://www.your-mind.com/web/product/big/202604/0d2299809f787814410f7ae5f5342e3c.png",
       "cover": "https://www.your-mind.com/web/product/big/202604/0d2299809f787814410f7ae5f5342e3c.png",
       "aladin_search": "https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord=%5B%EC%9E%AC%EC%9E%85%EA%B3%A0%5D%20Carnet%20de%20dessin%20%C2%B7%20%EB%A5%98%EC%9D%80%EC%A7%80"
-    },
-    {
-      "title": "[4차 입고] 나는 정말 나무가 되었다 · 오하나, 홍시야",
-      "store": "유어마인드",
-      "section": "BOOKS",
-      "store_link": "https://www.your-mind.com/product/4차-입고-나는-정말-나무가-되었다-·-오하나-홍시야/5971/category/27/display/1/",
-      "store_cover": "https://www.your-mind.com/web/product/big/202604/6676396b7fcfa5cbdc44d2f0ae409437.png",
-      "cover": "https://www.your-mind.com/web/product/big/202604/6676396b7fcfa5cbdc44d2f0ae409437.png",
-      "aladin_search": "https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord=%5B4%EC%B0%A8%20%EC%9E%85%EA%B3%A0%5D%20%EB%82%98%EB%8A%94%20%EC%A0%95%EB%A7%90%20%EB%82%98%EB%AC%B4%EA%B0%80%20%EB%90%98%EC%97%88%EB%8B%A4%20%C2%B7%20%EC%98%A4%ED%95%98%EB%82%98%2C%20%ED%99%8D%EC%8B%9C%EC%95%BC"
     },
     {
       "title": "[예약판매/배송비 무료] 숨겨둔 책 세트 (No.025, 026, 027)",
@@ -1820,7 +1820,8 @@ window.__INDIE_BOOKS_DATA__ = {
       "week": "2026년 7월 20일 ~ 7월 26일",
       "tb_page": "http://thanksbooks.com/site/thanksbooks/index.php/selection/weeklttb2026/",
       "store_cover": "",
-      "cover": "",
+      "cover": "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791191347975.jpg",
+      "buy_link": "https://product.kyobobook.co.kr/detail/S000001807570",
       "aladin_search": "https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord=%EC%9A%B0%EB%A6%AC%EB%8A%94%20%EC%82%AC%EB%9E%91%ED%95%98%EA%B8%B0%20%EC%A2%8B%EC%9D%80%20%ED%8C%94%EC%9D%84%20%EA%B0%80%EC%A1%8C%EA%B5%AC%EB%82%98%28%ED%95%9C%EC%A7%80%20%EC%97%90%EB%94%94%EC%85%98%29"
     },
     {
@@ -2271,8 +2272,8 @@ window.__INDIE_BOOKS_DATA__ = {
   "stores_attempted": [
     "유어마인드",
     "북앤스페이스",
-    "사적인서점",
     "고요서사",
-    "소심한책방"
+    "소심한책방",
+    "사적인서점"
   ]
 };
